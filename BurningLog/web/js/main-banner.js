336311cac7949c6f6cb420b7a5ab6f19b5268844
Anh Huy <huykingnice@gmail.com>
2022-02-16 14:32:19 +0700

    Theme buring

---
+++ BurningLog/web/js/main-banner.js
@@ -0,0 +1,16 @@
+require([
+   'jquery',
+   'slick',
+   'domReady!'
+], function ($) {
+   $(".main-banner-slider").slick({
+      dots: false,
+      prevArrow: false,
+      nextArrow: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1
+   });
+});
+
